859d3dbc271720d2d40aab55a3ed2fa45b2e4b92
benton.alexander@yahoo.com <benton.alexander@yahoo.com>
2025-06-02 19:57:26 +0000

    test

--- models/example/apply_changes.sql
+++ models/example/apply_changes.sql
@@ -1,17 +1,1 @@
-{% set source_table = source('public', 'table1') %}
-{% set source_stream = source('public', 'table1_stream') %}
-
-{% if is_incremental() %}
-
-    -- Incremental run: get only INSERT and UPDATE actions from stream
-    SELECT *
-    FROM {{ source_stream }}
-    WHERE metadata$action IN ('INSERT', 'UPDATE')
-
-{% else %}
-
-    -- Full-refresh: load entire base table
-    SELECT *
-    FROM {{ source_table }}
-
-{% endif %}
+select 1 as id
