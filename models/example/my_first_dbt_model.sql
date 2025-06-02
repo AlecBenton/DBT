76272bd88e90649031fd0390bc3c36d5bb4e9282
benton.alexander@yahoo.com <benton.alexander@yahoo.com>
2025-06-02 20:00:53 +0000

    changed example

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,27 +1,17 @@
+{% set source_table = source('public', 'table1') %}
+{% set source_stream = source('public', 'table1_stream') %}
 
-/*
-    Welcome to your first dbt model!
-    Did you know that you can also configure models directly within SQL files?
-    This will override configurations stated in dbt_project.yml
+{% if is_incremental() %}
 
-    Try changing "table" to "view" below
-*/
+    -- Incremental run: get only INSERT and UPDATE actions from stream
+    SELECT *
+    FROM {{ source_stream }}
+    WHERE metadata$action IN ('INSERT', 'UPDATE')
 
-{{ config(materialized='table') }}
+{% else %}
 
-with source_data as (
+    -- Full-refresh: load entire base table
+    SELECT *
+    FROM {{ source_table }}
 
-    select 1 as id
-    union all
-    select null as id
-
-)
-
-select *
-from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+{% endif %}
